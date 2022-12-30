985c2a8afc8ff3c74e5c998d0b54e79b9b8271b5
j-hulbert <j-hulbert@users.noreply.github.com>
2022-12-30 00:01:11 -0500

    gh pages

--- docs/_next/static/k3fAy3XC59_akQ6U33t5l/buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/cd7ef803f9f3ffc7.css","static/chunks/pages/index-b4da966a587d8d4e.js"],"/_error":["static/chunks/pages/error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
