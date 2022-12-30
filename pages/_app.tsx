d69b1d9311d72380a10b6dc1392d57b6c7167b36
j-hulbert <j-hulbert@users.noreply.github.com>
2022-12-29 22:57:57 -0500

    gh pages setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import { Space_Mono } from "@next/font/google";
 import type { AppProps } from "next/app";
 
-const space_mono = Space_Mono({ weight: "700" });
+const space_mono = Space_Mono({ weight: ["400", "700"], subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
